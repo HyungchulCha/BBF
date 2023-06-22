5b0e1ebc67db9b72bdd96b024b4b2442e3f967a7
ckgudcjf <ckgudcjf@gmail.com>
2023-06-22 19:41:22 +0900

    ing

--- b.py
+++ b.py
@@ -30,31 +30,14 @@ class BotBinanceFutures():
         send_text = ''
         send_text = send_text + f'{tk}'
 
-        df_30 = get_bnc_df(self.bnc, tk, '30m', 120)
-
-        if not df_30 is None:
-            df_30['macd'], _, _ = indicator_macd(df_30['close'], 12, 26, 9)
-            df_30_t = df_30.tail(3).head(2)
-            macd_1 = df_30_t['macd'].iloc[-2]
-            macd_0 = df_30_t['macd'].iloc[-1]
-            macd_d = 'Rise' if macd_1 < macd_0 else 'Fall'
-            send_text = send_text + f'\nM : {round(macd_1, 8)} → {round(macd_0, 8)} {macd_d}'
-
-        df = self.get_unrealized_df(get_bnc_df(self.bnc, tk, '15m', 4*24*7))
+        df = self.get_unrealized_df(get_bnc_df(self.bnc, tk, '15m', 4*24*14))
         ul = []
 
         if not df is None:
 
             df.drop(df.tail(1).index,inplace=True)
-
-            # df_start = df.head(1).index[0]
-            # df_end = df.tail(1).index[0]
             close_last = float(df.tail(1)['close'].iloc[-1])
-
-            # send_text = send_text + f'\nP : {df_start} ~ {df_end}'
-
-            df_len = len(df)
-            cnt = 1
+            
             for i, r in df.iterrows():
 
                 _time = i.strftime('%Y-%m-%d %H:%M:%S')
@@ -74,8 +57,6 @@ class BotBinanceFutures():
                 direct_prev_1 = r['direct_prev_1']
                 direct_prev_2 = r['direct_prev_2']
 
-                is_last = cnt == df_len
-
                 if len(ul) > 0:
                     for l in ul:
                         if (l['e'] == '') & (self.is_in_range(float(l['tp']), low, high)):
@@ -84,40 +65,28 @@ class BotBinanceFutures():
                 if direct_prev_0 == direct_prev_1:
                     if direct_prev_0:
                         ul.append({'s': _time, 'e': '', 'c': '2', 'ot': 'l', 'op': close, 'tp': tp_p})
-                        if (macd_0 > 0) & is_last:
-                            send_text = send_text + f'\nN : 2-L - TP {round(tp_p, 8)}'
                     else:
                         ul.append({'s': _time, 'e': '', 'c': '2', 'ot': 's', 'op': close, 'tp': tp_m})
-                        if (macd_0 < 0) & is_last:
-                            send_text = send_text + f'\nN : 2-S - TP {round(tp_m, 8)}'
                 
                 if direct_prev_0 == direct_prev_1 == direct_prev_2:
                     if direct_prev_0:
                         ul.append({'s': _time, 'e': '', 'c': '3', 'ot': 's', 'op': close, 'tp': tp_m})
-                        if (macd_0 < 0) & is_last:
-                            send_text = send_text + f'\nN : 3-S - TP {round(tp_m, 8)}'
                     else:
                         ul.append({'s': _time, 'e': '', 'c': '3', 'ot': 'l', 'op': close, 'tp': tp_p})
-                        if (macd_0 > 0) & is_last:
-                            send_text = send_text + f'\nN : 3-L - TP {round(tp_p, 8)}'
                 
-                cnt = cnt + 1
-
             _ul_l = [_l for _l in ul if ((_l['e'] == '') & (_l['ot'] == 'l'))]
             _ul_s = [_l for _l in ul if ((_l['e'] == '') & (_l['ot'] == 's'))]
+            _ul_l_len = len(_ul_l)
+            _ul_s_len = len(_ul_s)
 
-            send_text = send_text + f'\nS : S {len(_ul_s)} / L {len(_ul_l)}'
+            send_text = send_text + f'\nS {_ul_s_len} / L {_ul_l_len}'
 
             near_price_up = []
             near_price_dn = []
 
             if len(_ul_l) > 0:
-
                 for _ull in _ul_l:
-
                     tp = _ull['tp']
-                    # send_text = send_text + f'\n* {c}-{ot} : TP {round(tp, 8)}'
-
                     if close_last > tp:
                         near_price_dn.append(tp)
                     elif close_last < tp:
@@ -126,26 +95,21 @@ class BotBinanceFutures():
             if len(_ul_s) > 0:
 
                 for _uls in _ul_s:
-
                     tp = _uls['tp']
-                    # send_text = send_text + f'\n* {c}-{ot} : TP {round(tp, 8)}'
-
                     if close_last > tp:
                         near_price_dn.append(tp)
                     elif close_last < tp:
                         near_price_up.append(tp)
 
-            send_text = send_text + f'\nO :'
             if len(near_price_dn) > 0:
                 if len(near_price_dn) >= 2:
-                    near_price_dn = sorted(near_price_dn)[-2:]
-                send_text = send_text + f' {near_price_dn} <'
-            send_text = send_text + f' C'
+                    near_price_dn = sorted(near_price_dn)
+                send_text = send_text + f'\n{near_price_dn}'
+            send_text = send_text + f'\nC'
             if len(near_price_up) > 0:
-                print(near_price_up)
                 if len(near_price_up) >= 2:
-                    near_price_up = sorted(near_price_up)[:2]
-                send_text = send_text + f' < {near_price_up}'
+                    near_price_up = sorted(near_price_up)
+                send_text = send_text + f'\n{near_price_up}'
 
             return send_text
         
